462351de51f035e3aa098e4acb11d833b3ba10fe
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-12-05 10:32:03 +0000

    Login view function: Is added

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -2,6 +2,7 @@
 """Home page route """
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -22,3 +23,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
