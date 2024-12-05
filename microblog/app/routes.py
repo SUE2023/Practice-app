7ef8fd266bf297cc933c6da6d32d5a8904ce221c
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-12-05 12:02:22 +0000

    Links:Use of url_for() function for links

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -27,8 +27,8 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-     if form.validate_on_submit():
+    if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
