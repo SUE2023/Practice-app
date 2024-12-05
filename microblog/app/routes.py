c52c3588c4f442797320d89b27501e5884064f7a
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-12-05 09:15:35 +0000

    List of posts in view function: To test the application since users function not yet  created

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -7,4 +7,18 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Mercy'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'Veronica'},
+            'body': 'Experience of 2024 Retreats!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Experience of Leadership!'
+        },
+         {
+            'author': {'username': 'John Paul'},
+            'body': 'All things are gifts!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
