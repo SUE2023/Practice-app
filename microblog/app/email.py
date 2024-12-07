bdc9806a390a2dac1b9f0fa4a072ab9eeb03115f
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-12-07 19:46:37 +0000

    Send emails asynchronously: Added

--- microblog/app/email.py
+++ microblog/app/email.py
@@ -4,6 +4,7 @@ from flask_mail import Message
 from app import mail
 from flask import render_template
 from app import app
+from threading import Thread
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
@@ -20,4 +21,16 @@ def send_password_reset_email(user):
                text_body=render_template('email/reset_password.txt',
                                          user=user, token=token),
                html_body=render_template('email/reset_password.html',
-                                         user=user, token=token))
+                                         user=user, token=tokne))
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    Thread(target=send_async_email, args=(app, msg)).start()
