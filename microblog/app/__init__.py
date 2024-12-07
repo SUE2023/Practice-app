2129f742a351ea2a1eaf2daece2f85dd0f5ea7e2
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-12-07 20:53:24 +0000

    Initialize Flask-Babel: Added

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Flask application instance """
+from flask import request
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -11,7 +12,11 @@ from logging.handlers import RotatingFileHandler
 import os
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_babel import Babel
 
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -21,6 +26,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 moment = Moment(app)
+babel = Babel(app, locale_selector=get_locale)
 
 
 if not app.debug:
