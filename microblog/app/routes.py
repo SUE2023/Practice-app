b30df9f5e9394a1064def8c30ea0aff06ae0a3c5
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-12-07 17:22:57 +0000

    Post submission form in index view function: Added

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -10,6 +10,8 @@ from app.models import User
 from datetime import datetime, timezone
 from app.forms import EditProfileForm
 from app.forms import EmptyForm
+from app.forms import PostForm
+from app.models import Post
 
 
 @app.before_request
@@ -19,10 +21,17 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
     posts = [
         {
             'author': {'username': 'John'},
@@ -33,7 +42,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', posts=posts)
+    return render_template('index.html', title='Home', form=form, posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
