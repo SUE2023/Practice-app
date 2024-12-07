8d496424e086ca93f29a3eecdb46ee61d9af5619
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-12-07 12:16:59 +0000

    Many-to-many followers relationship: Added

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -27,6 +27,15 @@ class User(UserMixin, db.Model):
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    following: so.WriteOnlyMapped['User'] = so.relationship(
+        secondary=followers, primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        back_populates='followers')
+    followers: so.WriteOnlyMapped['User'] = so.relationship(
+        secondary=followers, primaryjoin=(followers.c.followed_id == id),
+        secondaryjoin=(followers.c.follower_id == id),
+        back_populates='following')
+
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author')
 
